a710cd19e540bac0accdd46ea0012a0d95e8660e
Ilya <salvadordali2303@gmail.com>
2017-09-25 18:59:07 +0300

    first part of JS

---
+++ script.js
@@ -0,0 +1,108 @@
+"use strict";
+var numOfRow = document.querySelector('.row:last-child').className.split('-')[1];
+function onMouseApp(out) {
+    if(out){
+        let delBtns = document.querySelectorAll('.btn-del');
+        delBtns.forEach((btn,i,delBtns) => btn.style.display = 'none');
+        document.querySelector('.app').style.top = window.innerHeight*0.3 + 'px';
+        document.querySelector('.app').style.left = window.innerWidth*0.4 + 'px';
+        document.querySelector('.btn-add-row').style.marginLeft = '6px';
+    }else{
+        let delBtns = document.querySelectorAll('.btn-del');
+        delBtns.forEach((btn,i,delBtns) => btn.style.display = 'block')
+        document.querySelector('.app').style.top = window.innerHeight*0.3 - 56 + 'px';
+        document.querySelector('.app').style.left = window.innerWidth*0.4 - 56 + 'px';
+        document.querySelector('.btn-add-row').style.marginLeft = '62px';
+    }
+}
+
+document.querySelector('.app').onmouseover = handler;
+function handler(event) {
+    let elem = event.target;
+    onMouseApp(false);
+    if(elem.className.indexOf('col') >= 0 && elem.className.indexOf('btn') < 0){
+        let numOfRow = elem.parentNode.className.split('-')[1];
+        let delRowBtnMarginTop = 6*numOfRow + 48*(numOfRow - 1) + 'px';
+        document.querySelector('.btn-del-row').style.marginTop = delRowBtnMarginTop;
+        let numOfCol = elem.className.split('-')[1];
+        document.querySelector('.btn-del-row').classList.remove(document.querySelector('.btn-del-row').classList[3]);
+        document.querySelector('.btn-del-row').classList.add('btn-del-row-' + numOfRow);
+        let delColBtnMargin = 56 + 6*numOfCol + 48*(numOfCol - 1);
+        document.querySelector('.btn-del-col').style.margin = '3px 0 3px ' + delColBtnMargin + 'px';
+        document.querySelector('.btn-del-col').classList.remove(document.querySelector('.btn-del-col').classList[3]);
+        document.querySelector('.btn-del-col').classList.add('btn-del-col-' + numOfCol);
+
+    }
+
+}
+document.querySelector('.app').onmouseout = handlerOut;
+function handlerOut() {
+    onMouseApp(true);
+}
+document.querySelector('.btn-add-col').onclick = () => {
+    let rows = document.querySelectorAll('.row');
+    let cols = document.querySelectorAll('.col');
+
+    for(let i = 0;i < rows.length;i++){
+        let newCol = document.createElement('div');
+        let nextNumOfCol = 1 +  Number(cols[cols.length-1].className.split('-')[1]);
+        newCol.classList.add('col','col-' + nextNumOfCol);
+        rows[i].appendChild(newCol);
+    }
+}
+document.querySelector('.btn-add-row').onclick = () => {
+    let rows = document.querySelectorAll('.row');
+    let newRow = document.createElement('div');
+    let nextNumOfRow = 1 +  Number(rows[rows.length-1].className.split('-')[1]);
+    newRow.classList.add('row','row-' + nextNumOfRow);
+    for(let i = 0;i < rows[0].childNodes.length;i++){
+        if(rows[0].childNodes[i].nodeName === '#text') continue;
+        let newChild = document.createElement('div');
+        newChild.classList = rows[0].childNodes[i].classList
+        newRow.appendChild(newChild)
+    }
+    document.querySelector('.table').appendChild(newRow);
+
+}
+
+document.querySelector('.btn-del-col').onclick = () =>
+{
+    let numOfCol = document.querySelector('.col:last-child').className.split('-')[1];
+    let condidats = document.querySelectorAll('.col-' + numOfCol);
+    for (let i = 0; i < condidats.length; i++) {
+        condidats[i].parentNode.removeChild(condidats[i]);
+    }
+    if (document.querySelector('.btn-del-col').classList[3].split('-')[3] === numOfCol){
+        numOfCol--;
+        let delColBtnMargin = 56 + 6 * numOfCol + 48 * (numOfCol - 1);
+        document.querySelector('.btn-del-col').style.margin = '3px 0 3px ' + delColBtnMargin + 'px';
+        document.querySelector('.btn-del-col').classList.remove(document.querySelector('.btn-del-col').classList[3]);
+        document.querySelector('.btn-del-col').classList.add('btn-del-col-' + numOfCol);
+    }
+    if(numOfCol <= 1){
+        document.querySelector('.btn-del-col').style.display = 'none';
+        document.querySelector('.app').style.top = window.innerHeight*0.3 + 'px';
+    }
+}
+
+document.querySelector('.btn-del-row').onclick = () => {
+    let numOfRow = document.querySelector('.row:last-child').className.split('-')[1];
+    let condidat = document.querySelector('.row-' + numOfRow);
+    condidat.parentNode.removeChild(condidat);
+    if (document.querySelector('.btn-del-row').classList[3].split('-')[3] === numOfRow) {
+        numOfRow--;
+        let delRowBtnMarginTop = 6 * numOfRow + 48 * (numOfRow - 1) + 'px';
+        document.querySelector('.btn-del-row').style.marginTop = delRowBtnMarginTop;
+        document.querySelector('.btn-del-row').classList.remove(document.querySelector('.btn-del-row').classList[3]);
+        document.querySelector('.btn-del-row').classList.add('btn-del-row-' + numOfRow);
+    }else{
+        numOfRow--;
+    }
+    console.log(numOfRow)
+
+    if(numOfRow <= 1){
+        document.querySelector('.btn-del-row').style.display = 'none';
+        document.querySelector('.app').style.left = window.innerWidth*0.4 + 'px';
+        document.querySelector('.btn-add-row').style.marginLeft = '62px';
+    }
+}
