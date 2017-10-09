4d7a41d9e793c4485fc8274828b3589657e68670
Ilya <salvadordali2303@gmail.com>
2017-10-09 17:19:00 +0300

    second refactoring

--- script.js
+++ script.js
@@ -1,44 +1,49 @@
 "use strict";
-const TABLE_BLOCK_SIZE = 38;
-const TABLE_BORDER_SIZE = 2;
 class SuperTable {
-    constructor() {
-        this.app = document.querySelector('.app');
-        this.table = document.querySelector('.table');
-        this.btnDelRow = document.querySelector('.table__btn_position-left');
-        this.btnDelCol = document.querySelector('.table__btn_position-top');
-        this.btnAddCol = document.querySelector('.table__btn_position-right');
-        this.isVisibleBtnDelCol = 1;
+    constructor(elem) {
+        this.table = elem.querySelector('.ST-table').firstElementChild;
+        this.btnDelRow = elem.querySelector('.ST-table__btn_position-left');
+        this.btnDelCol = elem.querySelector('.ST-table__btn_position-top');
+        this.tableBlockSize = 44;
+        this.tableBorderSize = 3;
+        this.initTableJS(elem);
+    }
+    initTableJS(element){
+        element.querySelector('.ST-table').addEventListener('mouseover',(event) => this.moveBtns(event.target));
+        element.addEventListener('mouseleave',() => this.hideBtns(true));
+        element.addEventListener('mouseenter',() => this.hideBtns(false));
+        element.querySelector('.ST-btn_del-row').addEventListener('click',(event) => this.deleteRow(event.target.parentNode));
+        element.querySelector('.ST-btn_del-col').addEventListener('click',(event) => this.deleteCol(event.target.parentNode));
+        element.querySelector('.ST-btn_add-row').addEventListener('click',this.addRow.bind(this));
+        element.querySelector('.ST-btn_add-col').addEventListener('click',this.addCol.bind(this));
+
     }
     findNumOfChild(element){
         let parentArr = [].slice.apply(element.parentNode.children);
         return parentArr.indexOf(element);
     }
     moveBtnDelRow(element){
-        let marginTop = (this.findNumOfChild(element) + this.isVisibleBtnDelCol)*TABLE_BLOCK_SIZE + TABLE_BORDER_SIZE;
+        let marginTop = (this.findNumOfChild(element) + 1)*this.tableBlockSize + this.tableBorderSize;
         this.btnDelRow.style.marginTop = marginTop + 'px';
     }
     moveBtnDelCol(element){
-        let marginLeft = this.findNumOfChild(element)*TABLE_BLOCK_SIZE + TABLE_BORDER_SIZE;
+        let marginLeft = this.findNumOfChild(element)*this.tableBlockSize + this.tableBorderSize;
         this.btnDelCol.style.marginLeft = marginLeft + 'px';
     }
     hideBtnDelCol(val){
-        if(!val && this.table.children[0].children.length <= 1){
+        if(!val && this.table.firstElementChild.children.length <= 1){
             return;
         }
-        this.btnDelCol.hidden = val;
-        this.btnAddCol.style.marginTop = (TABLE_BLOCK_SIZE)*(!val) + TABLE_BORDER_SIZE + 'px';
-        this.app.style.marginTop = (TABLE_BLOCK_SIZE)*val + 'px';
+        this.btnDelCol.firstElementChild.hidden = val;
     }
     hideBtnDelRow(val){
         if(!val && this.table.children.length <= 1){
             return;
         }
-        this.btnDelRow.hidden = val;
-        this.app.style.marginLeft = (TABLE_BLOCK_SIZE)*val + 'px'
+        this.btnDelRow.firstElementChild.hidden = val;
     }
     moveBtns(element){
-        if(element.className !== 'table__col') return;
+        if(element.className !== 'ST-table__col') return;
         this.moveBtnDelCol(element);
         this.moveBtnDelRow(element.parentNode);
     }
@@ -48,9 +53,9 @@ class SuperTable {
     }
     deleteRow (element){
         let marginTop = element.style.marginTop.split('px')[0];
-        let rowNum = (marginTop - (TABLE_BORDER_SIZE + TABLE_BLOCK_SIZE))/TABLE_BLOCK_SIZE;
+        let rowNum = (marginTop - (this.tableBorderSize + this.tableBlockSize))/this.tableBlockSize;
         if(this.table.children.length - 1 === rowNum){
-            element.style.marginTop = marginTop - TABLE_BLOCK_SIZE + 'px';
+            element.style.marginTop = marginTop - this.tableBlockSize + 'px';
         }
         rowNum = rowNum >= 1 ? rowNum : 0;
         this.table.removeChild(this.table.children[rowNum]);
@@ -60,24 +65,20 @@ class SuperTable {
     }
     deleteCol (element){
         let marginLeft = element.style.marginLeft.split('px')[0];
-        let colNum = (marginLeft - (TABLE_BORDER_SIZE))/TABLE_BLOCK_SIZE;
-        if(this.table.children[0].children.length - 1 === colNum){
-            element.style.marginLeft = marginLeft - TABLE_BLOCK_SIZE + 'px';
+        let colNum = (marginLeft - (this.tableBorderSize))/this.tableBlockSize;
+        if(this.table.firstElementChild.children.length - 1 === colNum){
+            element.style.marginLeft = marginLeft - this.tableBlockSize + 'px';
         }
         colNum = colNum >= 1 ? colNum : 0;
         for(let i = 0;i < this.table.children.length;i++) {
             this.table.children[i].removeChild(this.table.children[i].children[colNum])
         }
-        if(this.table.children[0].children.length <= 1){
-            let marginTop = this.btnDelRow.style.marginTop.split('px')[0];
-            marginTop -= TABLE_BLOCK_SIZE;
-            this.btnDelRow.style.marginTop = marginTop + 'px';
-            this.hideBtnDelCol(true);
-            this.isVisibleBtnDelCol = 0;
+        if(this.table.firstElementChild.children.length <= 1){
+            this.hideBtnDelCol(true)
         }
     }
     addRow(){
-        let newRow = this.table.children[0].cloneNode(true);
+        let newRow = this.table.firstElementChild.cloneNode(true);
         for(let i = 0;i < newRow.children.length;i++){
             newRow.children[i].innerHTML = '';
         }
@@ -85,30 +86,21 @@ class SuperTable {
         this.hideBtnDelRow(false);
     }
     addCol(){
-        console.log(this.table.children[0].children.length);
-        let sampleNewCol = this.table.children[0].children[0].cloneNode(false);
+        let sampleNewCol = this.table.firstElementChild.firstElementChild.cloneNode(false);
         sampleNewCol.innerHTML = "";
         for(let i = 0; i < this.table.children.length;i++){
             let newCol = sampleNewCol.cloneNode(false);
             this.table.children[i].appendChild(newCol);
         }
-        if(this.table.children[0].children.length <= 2) {
-            let marginTop = Number(this.btnDelRow.style.marginTop.split('px')[0]);
-            marginTop += TABLE_BLOCK_SIZE;
-            this.btnDelRow.style.marginTop = marginTop + 'px';
+        if(this.table.firstElementChild.children.length <= 2) {
             this.hideBtnDelCol(false);
-            this.isVisibleBtnDelCol = 1;
         }
     }
 
 
 }
 
-let table = new SuperTable();
-document.querySelector('.table').onmouseover = (e) => table.moveBtns(e.target);
-document.querySelector('.app').onmouseleave = () => table.hideBtns(true);
-document.querySelector('.app').onmouseenter = () => table.hideBtns(false);
-document.querySelector('.btn-del-row').onclick = (e)=> table.deleteRow(e.target);
-document.querySelector('.btn-del-col').onclick = (e)=> table.deleteCol(e.target);
-document.querySelector('.btn-add-row').onclick = () => table.addRow();
-document.querySelector('.btn-add-col').onclick = () => table.addCol();
+let supertables = document.querySelectorAll('.supertable-app');
+for(let i = 0;i < supertables.length; i++){
+    new SuperTable(supertables[i]);
+}
